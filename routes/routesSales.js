5f71ec2ae9a926a7bd11953f5b800fa817d2c8fe
MeetaVishal <meetavishal@outlook.com>
2022-02-11 14:52:21 +0530

    changes in sales api

--- routes/routesSales.js
+++ routes/routesSales.js
@@ -5,33 +5,41 @@ const Vehicle = require("../db/models/vehicle");
 
 //api provides all the sales data related to vehicles
 router.get("/sales", async (req, res, next) => {
-  const vehicle = await Vehicle.find({ name: "BMW" });
-  res.json(vehicle).status(200);
+  try {
+    const sales = Sales.find();
+    res.json(sales).status(200);
+  } catch (error) {
+    res.json({ message: error }).status(500);
+  }
 });
 
+//api helps to add vehicle sales  data
 router.post("/addSale", async (req, res, next) => {
-  const vehicle = await Vehicle.find({ name: "BMW" });
+  const vehicleName = req.body.vehicleName;
+  const vehicle = await Vehicle.find({ name: vehicleName });
   const id = vehicle[0]._id;
   const sale = new Sales({
-    name: "Vishal Lade",
-    address: "Nagpur Maharashtra",
-    mobile: 67812345,
+    name: req.body.name,
+    address: req.body.address,
+    mobile: req.body.mobile,
     vehicleId: id,
   });
 
   try {
-    const v1 = await sale.save();
-    res.json(v1).status(200);
+    const sale = await sale.save();
+    res.json(sale).status(200);
   } catch (error) {
     console.log(error);
+    res.json({ message: error }).status(500);
   }
 });
 
-//api helps to update vehicle data
+//api helps to update vehicle sales  data
 router.patch("/updateSale", async (req, res, next) => {});
 
-//api helps to remove vehicle data
+//api helps to remove vehicle sales  data
 router.delete("/removeSale", (req, res, next) => {
   res.send("Hello api").status(200);
 });
+
 module.exports = router;
